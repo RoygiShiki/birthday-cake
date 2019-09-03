7cd51bce08a1ab5af5720918508bca07236019b2
unknown <330634431@qq.com>
2019-09-03 14:12:01 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,140 @@
+$('.start').click(function(){
+    $('.stage1').fadeOut();
+    fire_modal('./cake_modal.png','做个蛋糕吧!','雪儿对不起, 今年的生日没有在你身边了, 不过我还有一个月就来北方啦, 以后你的每个每个生日, 我都要待在你身边陪你好好庆祝, 这次就先这样陪你做个蛋糕吧, 略略略...');
+})
+
+progress = 1;
+
+$('.modal_content button').click(function(){
+    progress++;
+    close_modal(progress)
+})
+
+function close_modal(callback){
+    modal.css('transform','translateY(-50%) scale(0)')
+    setTimeout(function(){
+        $('.stage' + callback).fadeIn();
+    },600)
+}
+
+
+function fire_modal(imgurl,title,content){
+
+    modal = $('.birthday_inner__modal');
+    modal.find('h1').html(title);
+    modal.find('img').attr('src',imgurl);
+    modal.find('p').html(content);
+    setTimeout(function(){
+        modal.css('transform','translateY(-50%) scale(1)')
+    },1000)
+
+
+}
+
+
+mixing = false;
+mixtimes = 0;
+
+$('.mixer').click(function(){
+    if(mixing == false){
+        mixing = true
+        mixtimes++;
+        $('.mix_spoon img').addClass('move')
+        setTimeout(function(){
+            $('.mix_spoon img').removeClass('move')
+            mixing = false;
+        },1000)
+    }
+    if(mixtimes == 3){
+        $('.stage2').fadeOut();
+        fire_modal('./mix_modal.png','搅拌完成!','面团搅拌得完美啦, 现在把他放进烤箱里烤3秒钟');
+
+    }
+
+})
+
+$('.tin').draggable({
+    revert:true
+})
+$( ".oven" ).droppable({
+    drop: function( event, ui ) {
+        $('.stage3').fadeOut();
+        fire_modal('./oven_modal.png','烘焙完成!','烤好了蛋糕之后, 是时候给你的蛋糕加上你喜欢的口味跟奶油啦');
+    }
+})
+
+bases = 0;
+fillings = 0;
+
+$('.sponges .item_inner').click(function(){
+    $('.sponges').addClass('inactive')
+    $('.fillings').removeClass('inactive')
+    t = $(this).attr('class').split(' ').pop();
+    bases++
+    if(bases < 6){
+        add_sponge(t)
+    }
+})
+
+$('.fillings .item_inner').click(function(){
+    $('.fillings').addClass('inactive')
+    $('.sponges').removeClass('inactive')
+    f = $(this).attr('class').split(' ').pop();
+    fillings++
+    if(fillings < 7){
+        add_filling(f)
+    }
+})
+
+function add_sponge(t){
+
+    $('.cakemake').prepend('<div style="width:' + (200 - (bases * 20)) + 'px" class="sponge sponge-' + t + '"><div></div><div></div><div></div><div></div><div></div></div>')
+    $('.sponges h5 span').html(bases)
+}
+
+$('.startagain').click(function(){
+    $('.cakemake').html('<div class="base"></div>');
+    bases = 0;
+    fillings = 0;
+    $('.sponges h5 span').html(bases)
+    $('.fillings h5 span').html(fillings)
+     $('.fillings').removeClass('inactive')
+    $('.sponges').addClass('inactive')
+})
+function add_filling(f){
+
+    $('.cakemake').prepend('<div style="width:' + (200 - (bases * 20)) + 'px" class="filling filling-' + f + '"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>')
+    $('.fillings h5 span').html(fillings)
+}
+
+function fin(){
+    $('h1,h2,.options,.startagain,.add').fadeOut();
+    
+    setTimeout(function(){
+        $('.cakemake').fadeIn()
+        $('.cakemake').animate({'margin-top':'0px'})
+    },1000)
+    add_candle()
+    $('svg').addClass('text')
+}
+
+function add_candle(){
+    var stages = $('.cakemake > div').length;
+    var h = (stages/2) * 41 + 22 + 'px';
+    console.log(stages)
+    $('.cakemake').prepend('<div class="candle" ><img src="./candle.png" /></div>')
+    $('svg').show()
+	$('.loveU').fadeIn()
+    // setTimeout(function(){
+    //     $('.sa').fadeIn()
+    // },2200)
+    
+}
+
+$('.add').click(function(){
+    fin();
+})
+
+$('.sa').click(function(){
+    window.location.reload();
+})
